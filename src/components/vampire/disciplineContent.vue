99cfa1e246adbabd7305f74e7d48d68ae3fe4b16
paltefogt <paltefogt@gmail.com>
2020-04-05 12:39:32 -0400

    Update disciplineContent.vue
    added small styling just for sanity's sake

--- src/components/vampire/disciplineContent.vue
+++ src/components/vampire/disciplineContent.vue
@@ -1,44 +1,55 @@
 <template>
   <div>
-    <div class="mainContent">
+    <span class="mainContent">
       <div v-if="$store.state.curContent.title">
-        Discipline: {{ $store.state.curContent.title }}
+        <span style="font-weight:bold">Discipline: </span>
+        {{ $store.state.curContent.title }}
       </div>
       <div
         v-if="
           !$store.state.curContent.children && $store.state.curContent.power
         "
       >
-        Power: {{ $store.state.curContent.power }}
+        <span style="font-weight:bold">Power: </span
+        >{{ $store.state.curContent.power }}
       </div>
       <div v-if="$store.state.curContent.level">
-        Level: {{ $store.state.curContent.level }}
+        <span style="font-weight:bold">Level: </span>
+        {{ $store.state.curContent.level }}
       </div>
       <div v-if="$store.state.curContent.quote">
-        Quote: {{ $store.state.curContent.quote }}
+        <span style="font-weight:bold">Quote: </span
+        >{{ $store.state.curContent.quote }}
       </div>
       <div v-if="$store.state.curContent.summary">
-        Summary: {{ $store.state.curContent.summary }}
+        <span style="font-weight:bold">Summary: </span
+        >{{ $store.state.curContent.summary }}
       </div>
       <div v-if="$store.state.curContent.testPool">
-        TestPool: {{ $store.state.curContent.testPool }}
+        <span style="font-weight:bold">TestPool: </span
+        >{{ $store.state.curContent.testPool }}
       </div>
       <div v-if="$store.state.curContent.cost">
-        Cost: {{ $store.state.curContent.cost }}
+        <span style="font-weight:bold">Cost: </span
+        >{{ $store.state.curContent.cost }}
       </div>
       <div v-if="$store.state.curContent.errata">
-        Errata: {{ $store.state.curContent.errata }}
+        <span style="font-weight:bold">Errata: </span
+        >{{ $store.state.curContent.errata }}
       </div>
       <div v-if="$store.state.curContent.exceptionalLong">
-        Exceptional: {{ $store.state.curContent.exceptionalLong }}
+        <span style="font-weight:bold">Exceptional: </span
+        >{{ $store.state.curContent.exceptionalLong }}
       </div>
       <div v-if="$store.state.curContent.foci">
-        Foci: {{ $store.state.curContent.foci }}
+        <span style="font-weight:bold">Foci: </span
+        >{{ $store.state.curContent.foci }}
       </div>
       <div v-if="$store.state.curContent.focusDescriptor">
-        Description: {{ $store.state.curContent.focusDescriptor }}
+        <span style="font-weight:bold">Description: </span
+        >{{ $store.state.curContent.focusDescriptor }}
       </div>
-    </div>
+    </span>
   </div>
 </template>
 
@@ -58,4 +69,9 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.mainContent {
+  display: inline-block;
+  width: 500px;
+}
+</style>
