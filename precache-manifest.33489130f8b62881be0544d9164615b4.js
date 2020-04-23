d336d19ef70f19f9bf05e13acf5af17771341a06
paltefogt <paltefogt@gmail.com>
2020-04-23 18:26:12 -0400

    deploy

---
+++ precache-manifest.33489130f8b62881be0544d9164615b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0269acff57f4ee23653b",
+    "url": "/WoDVue/css/app.f90e408c.css"
+  },
+  {
+    "revision": "f4fd265e5b316a75e0a6",
+    "url": "/WoDVue/css/chunk-vendors.e9324374.css"
+  },
+  {
+    "revision": "0a99739d491cb2e7a566e49528e5d7d7",
+    "url": "/WoDVue/index.html"
+  },
+  {
+    "revision": "0269acff57f4ee23653b",
+    "url": "/WoDVue/js/app.39bafb52.js"
+  },
+  {
+    "revision": "f4fd265e5b316a75e0a6",
+    "url": "/WoDVue/js/chunk-vendors.3ac4c500.js"
+  },
+  {
+    "revision": "93c8fa2d291828a93d3082b8370f42ef",
+    "url": "/WoDVue/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/WoDVue/robots.txt"
+  }
+]);
